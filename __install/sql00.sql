fc3a382ac6f0fa07d5e20caf5c0441324d52a1ac
willianlaynes <willianlaynes@hotmail.com>
2014-05-07 16:38:50 -0300

    SQL - 000
    - Script SQL pré-cadastro.

--- __install/sql00.sql
+++ __install/sql00.sql
@@ -1,3 +1,27 @@
+CREATE TABLE cad_complemento (
+  id_cmp INTEGER UNSIGNED NOT NULL AUTO_INCREMENT,
+  cmp_cliente VARCHAR(7) NULL,
+  cmp_cliente_seq VARCHAR(2) NULL,
+  cmp_salario DOUBLE NULL,
+  cmp_salario_complementar DOUBLE NULL,
+  cmp_estado_civil VARCHAR(1) NULL,
+  cmp_estado_civil_tempo INTEGER NULL,
+  cmp_profissao VARCHAR(100) NULL,
+  cmp_experiencia_vendas INTEGER NULL,
+  cmp_patrimonio VARCHAR(1) NULL,
+  cmp_imovel_tempo INTEGER NULL,
+  cmp_valor_aluguel DOUBLE NULL,
+  cmp_emprego_tempo INTEGER NULL,
+  cmp_propaganda VARCHAR(1) NULL,
+  cmp_propaganda2 VARCHAR(1) NULL,
+  cmp_log VARCHAR(10) NULL,
+  cmp_data INTEGER NULL,
+  cmp_lastupdate_log VARCHAR(10) NULL,
+  cmp_lastupdate INTEGER NULL,
+  cmp_status VARCHAR(1) NULL DEFAULT 1,
+  PRIMARY KEY(id_cmp)
+);
+
 CREATE TABLE cad_endereco (
   id_end INTEGER UNSIGNED NOT NULL AUTO_INCREMENT,
   end_cliente VARCHAR(7) NULL,
@@ -24,6 +48,7 @@ CREATE TABLE cad_pessoa (
   pes_rg VARCHAR(15) NULL,
   pes_nasc INTEGER NULL,
   pes_naturalidade VARCHAR(30) NULL,
+  pes_genero VARCHAR(1) NULL,
   pes_pai VARCHAR(100) NULL,
   pes_mae VARCHAR(100) NULL,
   pes_avalista VARCHAR(1) NULL DEFAULT P,
@@ -69,29 +94,6 @@ CREATE TABLE cad_telefone (
   PRIMARY KEY(id_tel)
 );
 
-CREATE TABLE cad_complemento (
-  id_cmp INTEGER UNSIGNED NOT NULL AUTO_INCREMENT,
-  cmp_cliente VARCHAR(7) NULL,
-  cmp_cliente_seq VARCHAR(2) NULL,
-  cmp_salario DOUBLE NULL,
-  cmp_salario_complementar DOUBLE NULL,
-  cmp_estado_civil VARCHAR(1) NULL,
-  cmp_estado_civil_tempo INTEGER NULL,
-  cmp_profissao VARCHAR(100) NULL,
-  cmp_experiencia_vendas INTEGER NULL,
-  cmp_patrimonio VARCHAR(1) NULL,
-  cmp_imovel_tempo INTEGER NULL,
-  cmp_valor_aluguel DOUBLE NULL,
-  cmp_emprego_tempo INTEGER NULL,
-  cmp_propaganda VARCHAR(1) NULL,
-  cmp_propaganda2 VARCHAR(1) NULL,
-  cmp_log VARCHAR(10) NULL,
-  cmp_data INTEGER NULL,
-  cmp_lastupdate_log VARCHAR(10) NULL,
-  cmp_lastupdate INTEGER NULL,
-  cmp_status VARCHAR(1) NULL DEFAULT 1,
-  PRIMARY KEY(id_cmp)
-);
 
 
 
