c6f9013738b4bdbe4af1c72ddfaffc9ef75d7b7b
willianlaynes <willianlaynes@hotmail.com>
2014-05-07 15:46:34 -0300

    SQL - 001
    - Estrutura do Banco de Dados

---
+++ __install/sql00.sql
@@ -0,0 +1,71 @@
+CREATE TABLE cad_endereco (
+  id_end INTEGER UNSIGNED NOT NULL AUTO_INCREMENT,
+  end_cliente VARCHAR(7) NULL,
+  end_rua VARCHAR(100) NULL,
+  end_numero VARCHAR(10) NULL,
+  end_bairro VARCHAR(30) NULL,
+  end_cidade VARCHAR(30) NULL,
+  end_estado VARCHAR(2) NULL,
+  end_cep VARCHAR(8) NOT NULL,
+  end_latitude VARCHAR(20) NULL,
+  end_longitude VARCHAR(20) NULL,
+  end_complemento VARCHAR(30) NULL,
+  end_status VARCHAR(1) NULL,
+  PRIMARY KEY(id_end)
+);
+
+CREATE TABLE cad_pessoa (
+  id_pes INTEGER UNSIGNED NOT NULL AUTO_INCREMENT,
+  pes_codigo VARCHAR(7) NULL,
+  pes_cliente VARCHAR(7) NULL,
+  pes_cliente_seq VARCHAR(2) NULL DEFAULT 00,
+  pes_nome VARCHAR(100) NULL,
+  pes_cpf VARCHAR(11) NULL,
+  pes_rg VARCHAR(15) NULL,
+  pes_nasc INTEGER NULL,
+  pes_naturalidade VARCHAR(30) NULL,
+  pes_pai VARCHAR(100) NULL,
+  pes_mae VARCHAR(100) NULL,
+  pes_avalista VARCHAR(1) NULL DEFAULT P,
+  pes_avalista_cod VARCHAR(7) NULL,
+  pes_data INTEGER NULL,
+  pes_lastupdate INTEGER NULL,
+  pes_status VARCHAR(1) NULL DEFAULT @,
+  PRIMARY KEY(id_pes)
+);
+
+CREATE TABLE cad_referencia (
+  id_ref INTEGER UNSIGNED NOT NULL AUTO_INCREMENT,
+  ref_cliente VARCHAR(7) NULL,
+  ref_cliente_seq VARCHAR(2) NULL,
+  ref_nome VARCHAR(30) NULL,
+  ref_cep VARCHAR(8) NULL,
+  ref_observacao TEXT NULL,
+  ref_data INTEGER NULL,
+  ref_grau VARCHAR(1) NULL,
+  ref_status VARCHAR(1) NULL DEFAULT @,
+  ref_ativo VARCHAR(1) NULL DEFAULT 1,
+  PRIMARY KEY(id_ref)
+);
+
+CREATE TABLE cad_referencia_tipo (
+  ret_codigo VARCHAR(7) NOT NULL,
+  id_ret INTEGER UNSIGNED NOT NULL AUTO_INCREMENT,
+  ret_nome VARCHAR(30) NULL,
+  ret_status VARCHAR(1) NULL,
+  PRIMARY KEY(ret_codigo)
+);
+
+CREATE TABLE cad_telefone (
+  id_tel INTEGER UNSIGNED NOT NULL AUTO_INCREMENT,
+  tel_cliente VARCHAR(7) NULL,
+  tel_cliente_seq VARCHAR(2) NULL,
+  tel_ddd VARCHAR(3) NULL,
+  tel_numero VARCHAR(9) NULL,
+  tel_tipo VARCHAR(1) NULL,
+  tel_data INTEGER UNSIGNED NULL,
+  tel_validado VARCHAR(1) NULL,
+  tel_status VARCHAR(1) NULL,
+  PRIMARY KEY(id_tel)
+);
+
