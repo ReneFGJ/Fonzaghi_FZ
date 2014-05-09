2f890f0e1034aea757ea2391361fa4bbb664b17a
willianlaynes <willianlaynes@hotmail.com>
2014-05-09 11:08:28 -0300

    Pre-Cadastro
    Detalhes do menu.

--- pre_cadastro/pre_cad_menu.php
+++ pre_cadastro/pre_cad_menu.php
@@ -5,7 +5,7 @@
         </div>    	
         
     	<div id="div1_new" class="textmenu_new bg1" style="display:none;"> DADOS PESSOAIS </div>
-    	<div id="div2_new" class="textmenu_new bg2" style="display:none;"> ENDEREÇO </div>
+    	<div id="div2_new" class="textmenu_new bg2" style="display:none;"> ENDEREǇO </div>
         <div id="div3_new" class="textmenu_new bg3" style="display:none;"> ESTADO CIVIL </div>
         <div id="div4_new" class="textmenu_new bg4" style="display:none;"> PROFISSIONAL </div>
         <div id="div5_new" class="textmenu_new bg5" style="display:none;"> REFERÊNCIAS </div>
