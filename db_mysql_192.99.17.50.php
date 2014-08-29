bfa25bd3fae0f8caadd81bd1c8d3cca005904912
Rene Faustino Gabriel Junior <renefgj@gmail.com>
2014-08-29 18:01:08 -0300

    Atualizacoes

---
+++ db_mysql_192.99.17.50.php
@@ -0,0 +1,18 @@
+<?php
+		$base = "mysql";
+		$base_user="brapciin_sa";
+		$base_port = '8130';
+		$base_host="localhost";
+		$base_name="brapciin_brapci_base";
+		$base_pass="448545ct";
+		
+		$db_base = 'brapciin_brapci_base.';
+		$db_public = 'brapciin_brapci_public.';
+		$db_apoio = 'brapciin_brapci_base.';
+		$db_diris = 'brapciin_brapci_diris.';		
+		
+		$http = 'http://www.brapci.inf.br/';
+		$ok = db_connect();
+		
+		header ('Content-type: text/html; charset=ISO-8859-1');
+?>
