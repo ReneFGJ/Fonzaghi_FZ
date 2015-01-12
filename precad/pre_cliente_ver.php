fe1f27ac594bf3eac335d3f26949ee7eaf61c363
willianlaynes <willianlaynes@hotmail.com>
2015-01-12 10:44:43 -0200

    v.0.15.03
    - remoção sisdebug

--- precad/pre_cliente_ver.php
+++ precad/pre_cliente_ver.php
@@ -1,6 +1,6 @@
 <?php
 require ("cab.php");
-require ($include . 'sisdoc_debug.php');
+//require ($include . 'sisdoc_debug.php');
 require ($include . 'sisdoc_data.php');
 require ('../_class/_class_form.php');
 require ("../_class/_class_cadastro_pre_analise.php");
